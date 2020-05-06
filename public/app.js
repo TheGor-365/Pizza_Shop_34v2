7a30bbda2ee73c685ac2d50899f479e0cf5b845b
Gor <7495500@gmail.com>
2020-05-06 18:28:01 +0300

    Add cart_get_orders

--- public/app.js
+++ public/app.js
@@ -16,6 +16,8 @@ function add_to_cart(id) {
   alert('Items in your cart: ' + cart_get_number_of_items())
 }
 
+
+
 function cart_get_number_of_items() {
   var cnt = 0;
   for(var i = 0; i < window.localStorage.length; i++) {
@@ -28,4 +30,19 @@ function cart_get_number_of_items() {
     }
   }
   return cnt;
+}
+
+function cart_get_orders() {
+  var orders = '';
+
+  for(var i = 0; i < window.localStorage.length; i++) {
+
+    var key = window.localStorage.key(i);
+    var value = window.localStorage.getItem(key);
+
+    if (key.indexOf('product_') ==0) {
+      orders = orders + key + '=' + value + ',';
+    }
+  }
+  return orders;
 }
